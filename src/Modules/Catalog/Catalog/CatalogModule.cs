46ce8503fd127ad05c66477d6f98a072896f8488
Nhathuy <phamvannhathuy160903@gmail.com>
2025-05-01 15:10:34 +0700

    feat: Implement Catalog module with Entity Framework Core; add Product model, DbContext, and data seeding functionality

--- src/Modules/Catalog/Catalog/CatalogModule.cs
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -8,13 +8,35 @@ namespace Catalog
     {
         public static IServiceCollection AddCatalogModule(this IServiceCollection services, IConfiguration configuration)
         {
+            // Add services to the container.
 
+            // Api Endpoint services
+            
+            // Application Use Case services
+
+            // Data - Infrastructure services
+            var connectionString = configuration.GetConnectionString("Database");
+
+            services.AddDbContext<CatalogDbContext>(options =>
+                options.UseNpgsql(connectionString));
+
+            services.AddScoped<IDataSeeder, CatalogDataSeeder>();
 
             return services;
         }
 
         public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
         {
+            // Configure the HTTP request pipeline.
+
+            // 1. Use Api Endpoint services
+
+            // 2. Use Application Use Case services
+
+            // 3. use Data - Infrastructure services
+
+            app.UseMigration<CatalogDbContext>();
+
             return app;
         }
     }
